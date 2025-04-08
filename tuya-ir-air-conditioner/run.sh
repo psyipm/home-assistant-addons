fd2310b10f9a4821fea4e82e178ab511633537b0
Igor Malinovskiy <ihor.malinovskiy@gmail.com>
2025-04-08 14:10:36 +0300

    Request mqtt credentials from home assistant

--- tuya-ir-air-conditioner/run.sh
+++ tuya-ir-air-conditioner/run.sh
@@ -7,9 +7,9 @@ export TUYA_SECRET_KEY=$(bashio::config 'tuya.secret_key')
 export TUYA_GATEWAY_ID=$(bashio::config 'device.ir_gateway_id')
 export TUYA_DEVICE_ID=$(bashio::config 'device.ac_device_id')
 
-export MQTT_SERVER_URL=$(bashio::config 'mqtt.server_url')
-export MQTT_USERNAME=$(bashio::config 'mqtt.username')
-export MQTT_PASSWORD=$(bashio::config 'mqtt.password')
+export MQTT_SERVER_URL=$(bashio::services mqtt 'host')
+export MQTT_USERNAME=$(bashio::services mqtt 'username')
+export MQTT_PASSWORD=$(bashio::services mqtt 'password')
 
 export MQTT_POWER_TOPIC=$(bashio::config 'mqtt_topics.power')
 export MQTT_MODE_TOPIC=$(bashio::config 'mqtt_topics.mode')
